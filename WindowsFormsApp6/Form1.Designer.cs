d252558058522e841d680cbe052adf5521cd0de9
Yigit <tanyelyigit@gmail.com>
2021-11-12 11:27:03 +0300

    first commit

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Öğrenci Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(43, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Öğrenci Soyadı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(43, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Doğum Tarihi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(43, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 31);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cinsiyet:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(46, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 31);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Sınıf:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(46, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 31);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Puan:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(159, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(212, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(212, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(121, 296);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(125, 24);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(225, 246);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(131, 345);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(574, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 90);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(574, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 90);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(574, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 90);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 401);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(675, 275);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 688);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //LİSTELE( SELECT )
+        void OgrenciListele()
+        {
+            baglanti = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=kutuphane;Integrated Security=True");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT * from ogrenci",baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("E");
+            comboBox1.Items.Add("K");
+
+            comboBox2.Items.Add("9B");
+            comboBox2.Items.Add("9C");
+            comboBox2.Items.Add("10A");
+            comboBox2.Items.Add("10B");
+            comboBox2.Items.Add("10C");
+            comboBox2.Items.Add("11A");
+            comboBox2.Items.Add("11C");
+
+            OgrenciListele();
+            
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text= dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            dateTimePicker1.Text= dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            comboBox2.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+        }
+
+        //EKLE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT into ogrenci(ograd,ogrsoyad,cinsiyet,dtarih,sinif,puan) values(@ograd,@ogrsoyad,@cinsiyet,@dtarih,@sinif,@puan)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@ograd", textBox2.Text);
+            komut.Parameters.AddWithValue("@ogrsoyad", textBox3.Text);
+            komut.Parameters.AddWithValue("@cinsiyet", comboBox1.Text);
+            komut.Parameters.AddWithValue("@dtarih", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@sinif", comboBox2.Text);
+            komut.Parameters.AddWithValue("@puan", textBox4.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            OgrenciListele();
+            MessageBox.Show("Kayıt başarıyla eklendi.");
+        }
+
+        //SİL
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE from ogrenci where ogrno=@ogrno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@ogrno", Convert.ToInt32(textBox1.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            OgrenciListele();
+            MessageBox.Show("Kayıt başarıyla silindi.");
+        }
+
+       //GÜNCELLE
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE ogrenci set ograd=@ograd,ogrsoyad=@ogrsoyad,cinsiyet=@cinsiyet,dtarih=@dtarih,sinif=@sinif,puan=@puan where ogrno=@ogrno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@ogrno", Convert.ToInt32(textBox1.Text));
+            komut.Parameters.AddWithValue("@ograd", textBox2.Text);
+            komut.Parameters.AddWithValue("@ogrsoyad", textBox3.Text);
+            komut.Parameters.AddWithValue("@cinsiyet", comboBox1.Text);
+            komut.Parameters.AddWithValue("@dtarih", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@sinif", comboBox2.Text);
+            komut.Parameters.AddWithValue("@puan", textBox4.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            OgrenciListele();
+            MessageBox.Show("Kayıt başarıyla güncellendi.");
+            // {0} nolu kayıt güncellendi nasıl yapılır?
+        }
+    }
+}
